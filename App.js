9a9c4b5a503ff4170cbf7b9049b4650cff4a68e3
Alibh <alibh619@gmail.com>
2018-02-28 18:20:07 +0530

    Initialized React native project

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class App extends Component {
+  state = {
+    placeName: ''
+  }
+
+  placeNameChangedHandler = (event) => {
+    this.setState({
+      placeName: event
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+        <TextInput 
+          style={{ width:300 }}
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
